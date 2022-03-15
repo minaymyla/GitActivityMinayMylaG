071274c92838ef23651cd10f1afd308c68e440fc
minaymyla <101568144+minaymyla@users.noreply.github.com>
2022-03-15 18:20:42 +0800

    Add files via upload

---
+++ git-python-minay.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 15:53:07 2022
+
+@author: malyn minay
+"""
+print ("Minay Myla G.")
+print ("Bangbangalon Boac Marinduque")
+print ("October 08, 2001")
+print ("Marinduque States College, BSI/T 2-B")
